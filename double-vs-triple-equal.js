9abf38c9019376de2a43098f3c2192b1428fcc44
Spartho <prantoshikder24@gmail.com>
2021-04-04 18:30:03 +0600

    advance JavaScript

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,34 @@
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+
+// const first = 2;
+// const second = 2;
+// if (first == second) {
+//     console.log("condition is true");
+// } else {
+//     console.log("condition is false");
+// }
+
+
+// const num1 = 2;
+// const num2 = "2";
+// if (num1 == num2) {
+//     console.log("condition is true");
+// } else {
+//     console.log("condition is false");
+// }
+
+
+// const first = 0;
+// const second = false;
+// if (first == second) {
+//     console.log("condition is true");
+// } else {
+//     console.log("condition is false");
+// }
